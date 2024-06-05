c66ea53ff937854321f9791852c47467aa1ceae3
todayIsDecember <timkivit@gmail.com>
2024-06-05 20:29:29 +0300

    add time ago

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,15 @@
 import { ICardBlog } from '../../interfaces/cardBlog.interface'
 import { CardBlog } from '../components'
 import styles from './page.module.css'
-import {format} from 'date-fns'
+import {format, formatDistanceToNow} from 'date-fns'
 
-export default function Blog() {
+export default function Blog(this: any) {
 
   const blogMock: ICardBlog[] = [
     {
       avatar: '/images/woman.jpg',
       name: 'Oksana Selepiy',
-      createdAt: format(new Date(), 'MMM dd, yyyy'),
+      createdAt: `${format(new Date(), 'MMM dd, yyyy')}, ${formatDistanceToNow(new Date(), {addSuffix: true})}`,
       title: 'Start Your Day Right: Delicious and Nutritious Breakfast Ideas',
       description: 'Discover a variety of mouthwatering and healthy breakfast recipes that will energize your morning and keep you fueled throughout the day. From quick and easy options to indulgent treats, find the perfect way to start your day.',
       views: 123,
@@ -20,7 +20,7 @@ export default function Blog() {
     {
       avatar: '/images/woman.jpg',
       name: 'Oksana Selepiy',
-      createdAt: format(new Date(), 'MMM dd, yyyy'),
+      createdAt: `${format(new Date(), 'MMM dd, yyyy')}, ${formatDistanceToNow(new Date(), {addSuffix: true})}`,
       title: 'Exploring the Joys of Cycling: Tips, Trails, and Gear for Every Rider',
       description: 'Discover the thrill of cycling with our comprehensive guide. From choosing the right gear to finding the best trails, we cover everything you need to know to enjoy your ride. Whether you`re a beginner or a seasoned cyclist, find tips and inspiration to make the most of your biking adventures.',
       views: 157,
@@ -31,7 +31,7 @@ export default function Blog() {
     {
       avatar: '/images/woman.jpg',
       name: 'Oksana Selepiy',
-      createdAt: format(new Date(), 'MMM dd, yyyy'),
+      createdAt: `${format(new Date(), 'MMM dd, yyyy')}, ${formatDistanceToNow(new Date(), {addSuffix: true})}`,
       title: 'The Importance of Morning Habits: Kickstart Your Day with Success',
       description: 'Establishing a morning routine can significantly enhance your productivity, mood, and overall well-being. By adopting positive habits such as exercise, healthy eating, and mindfulness practices, you can set a positive tone for the rest of your day, helping you to achieve your goals more effectively and maintain a balanced lifestyle. Discover the benefits of morning habits and how they can transform your life.',
       views: 276,
